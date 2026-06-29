92d822a6ead72251ac0203680e851e8febd7f8d7
Bernhard Fritz <bernhard.e.fritz@gmail.com>
2026-06-29 20:13:30 +0200

    feat: add isometric-cube example

---
+++ examples/isometric-cube/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"embed"
+
+	"github.com/bernhardfritz/two"
+)
+
+//go:embed resources/*
+var ASSETS embed.FS
+
+func init() {
+	two.AddFileSystem(ASSETS)
+}
+
+func main() {
+	topTexture := two.LoadTexture("resources/top.png")
+	sideTexture := two.LoadTexture("resources/side.png")
+
+	update := func(deltaTime, width, height, mouseX, mouseY float64, mouseButtons int) {
+		two.ClearBackground(255, 255, 255, 255)
+		cubeHeight := height / 2
+		// left side
+		two.SetTransform(1, 0.5, 0, 1, width/2-cubeHeight, cubeHeight/2)
+		two.SetTintColor(204, 204, 204, 255)
+		two.DrawTexture4f(sideTexture, 0, 0, cubeHeight, cubeHeight)
+		// right side
+		two.SetTransform(-1, 0.5, 0, 1, width/2+cubeHeight, cubeHeight/2)
+		two.SetTintColor(153, 153, 153, 255)
+		two.DrawTexture8f(sideTexture, 0, 0, cubeHeight, cubeHeight, sideTexture.Width, 0, -sideTexture.Width, sideTexture.Height)
+		// top
+		two.SetTransform(1, 0.5, -1, 0.5, width/2, 0)
+		two.SetTintColor(255, 255, 255, 255)
+		two.DrawTexture4f(topTexture, 0, 0, cubeHeight, cubeHeight)
+	}
+
+	two.SetGameLoop(update)
+}
