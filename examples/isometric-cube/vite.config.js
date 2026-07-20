65033cbd3ba6cc3f7b83f4638e5eaf76f780a325
Bernhard Fritz <bernhard.e.fritz@gmail.com>
2026-07-20 19:55:17 +0200

    refactor: rename vite plugin

--- examples/isometric-cube/vite.config.js
+++ examples/isometric-cube/vite.config.js
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite';
-import goWasm from '@bernhardfritz/two/rollup-plugin-go-wasm';
+import two from '@bernhardfritz/two/vite-plugin';
 
 export default defineConfig({
   plugins: [
-    goWasm(),
+    two(),
   ],
 });
